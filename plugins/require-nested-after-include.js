30bcbeddc233aa922976839264bfec7116b8c12d
E.T <ETLaurent@users.noreply.github.com>
2024-09-24 14:56:47 +0200

    add custom "require-nested-after-include" rule to enforce sass mixed-decls rule (#24)

---
+++ plugins/require-nested-after-include.js
@@ -0,0 +1,28 @@
+const stylelint = require('stylelint');
+
+const ruleName = 'custom/require-nested-after-include';
+const messages = stylelint.utils.ruleMessages(ruleName, {
+  expected: 'Expected "& { ... }" block after "@include" at-rule if declarations are present. See https://sass-lang.com/documentation/breaking-changes/mixed-decls'
+});
+
+module.exports = stylelint.createPlugin(ruleName, () => {
+  return (root, result) => {
+    root.walkRules((rule) => {
+      rule.walkAtRules('include', (atRule) => {
+        const nextNode = atRule.next();
+
+        if (nextNode?.type === 'decl') {
+          stylelint.utils.report({
+            message: messages.expected,
+            node: atRule,
+            result,
+            ruleName
+          });
+        }
+      });
+    });
+  };
+});
+
+module.exports.ruleName = ruleName;
+module.exports.messages = messages;
